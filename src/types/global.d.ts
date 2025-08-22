14a5c93ec7d18b085711321aeed73527e3701dec
ffacttt-hash <ffacttt@gmail.com>
2025-08-22 18:11:25 +0530

    Update global.d.ts

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -26,3 +26,20 @@ interface SpeechRecognitionAlternative {
   readonly confidence: number;
 }
 
+
+
+interface SpeechRecognitionErrorEvent extends Event {
+  readonly error: SpeechRecognitionErrorCode;
+  readonly message: string;
+}
+
+enum SpeechRecognitionErrorCode {
+  "no-speech" = "no-speech",
+  "aborted" = "aborted",
+  "audio-capture" = "audio-capture",
+  "network" = "network",
+  "not-allowed" = "not-allowed",
+  "service-not-allowed" = "service-not-allowed",
+  "bad-grammar" = "bad-grammar",
+  "language-not-supported" = "language-not-supported",
+  }
